62e259bfce45da5b47dfdab572f1fc3e9bf4f196
Matvey Smorodin <mtv.smorodin@gmail.com>
2023-12-05 17:57:33 +0300

    Create User entity

---
+++ src/main/java/com/currantino/taskapp/user/entity/User.java
@@ -0,0 +1,37 @@
+package com.currantino.taskapp.user.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id", nullable = false)
+    private Long id;
+
+    @Column(name = "user_email",
+            nullable = false,
+            unique = true,
+            length = 127)
+    private String email;
+
+    @Column(name = "user_password",
+            nullable = false,
+            length = 127)
+    private String password;
+
+}
