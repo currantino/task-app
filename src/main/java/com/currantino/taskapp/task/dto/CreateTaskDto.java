bc4fede3fd04a25ed068d5b6236fe0471516b144
Matvey Smorodin <mtv.smorodin@gmail.com>
2023-12-11 01:52:45 +0300

    Delete unused methods and fix typos

--- src/main/java/com/currantino/taskapp/task/dto/CreateTaskDto.java
+++ src/main/java/com/currantino/taskapp/task/dto/CreateTaskDto.java
@@ -7,7 +7,7 @@ import jakarta.validation.constraints.NotNull;
 public record CreateTaskDto(
         @NotBlank(message = "Task name must not be blank.")
         String name,
-        @NotBlank(message = "Task description must not be blnak")
+        @NotBlank(message = "Task description must not be blank")
         String description,
         @NotNull(message = "Task priority must not be null.")
         TaskPriority priority,
